613f19f94324c8cd2dee1758f849451793f597ea
Sugam <mrsugam@gmail.com>
2025-01-23 22:22:59 +0530

    Update ContentManagement.sol
    removed space

--- ContentManagement.sol
+++ ContentManagement.sol
@@ -9,15 +9,12 @@ contract ContentManagement {
     }
 
     Content[] public contents;
-
     event ContentCreated(uint256 indexed contentId, address indexed author, string uri, uint256 timestamp);
-
     function createContent(string memory _uri) public {
         uint256 contentId = contents.length;
         contents.push(Content(msg.sender, _uri, block.timestamp));
         emit ContentCreated(contentId, msg.sender, _uri, block.timestamp);
     }
-
     function getContent(uint256 _contentId) public view returns (Content memory) {
         require(_contentId < contents.length, "Content does not exist");
         return contents[_contentId];
